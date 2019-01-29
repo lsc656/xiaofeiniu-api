e685659c1d11a054f516dc27d995ac0929313abe
lsc656 <854332104@qq.com>
2019-01-29 09:13:18 +0800

    dwq

--- pool.js
+++ pool.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql')
+/*
+*MySQL数据库连接池
+*/
+const mysql = require('mysql');
 var pool = mysql.createPool({
-  host:'localhost',
-  port:'3306',
-  user:'root',
-  password:'',
-  database:'xiaofeiniu',
-  connectionLimit:20
-})
+  host: '127.0.0.1',      //数据库地址
+  port: 3306,             //数据库端口
+  user: 'root',           //数据库管理员
+  password: '',           //数据库管理员密码
+  database: 'xiaofeiniu', //默认连接的数据库
+  connectionLimit: 10     //连接池中连接数量
+});
 module.exports = pool;
